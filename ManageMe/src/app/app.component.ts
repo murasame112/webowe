024450d91862989ff291f87fad167bf7c3f39261
murasame112 <tomaszwiesek00@gmail.com>
2023-04-28 13:06:23 +0200

    serwis projektów, dodawanie i wyświetlanie ich z LS

--- ManageMe/src/app/app.component.ts
+++ ManageMe/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ManageMe';
+
+  public saveData(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  ngOnInit(): void {
+    localStorage.clear();
+    this.saveData('p1', 'costam projekt 1');
+    this.saveData('p2', '2 projekt');
+    this.saveData('f1', 'to juz funk');
+  }
+
+  
 }
---
+++ ManageMe/src/app/project.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  constructor() { }
+
+  public saveProject(value: string) {
+    let key = 'p';
+    let highest = '';
+    let highestId = 0;
+    let newHighestId = 0;
+    let storage: any = {},
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while ( i-- ) {
+        storage[keys[i]] =  localStorage.getItem(keys[i]);
+    }
+    for (const [key, value] of Object.entries(storage)) {
+      if(key.startsWith('p')){
+        highest = key;
+        highest = highest.slice(1);
+        newHighestId = parseInt(highest, 10);
+        if(newHighestId > highestId){
+          highestId = newHighestId;
+        }      
+      }
+      
+    }
+    highestId++;
+    key += highestId;
+    localStorage.setItem(key, value);
+  }
+
+  
+  getProjects(){
+    if(localStorage.length == 0){
+        //return false;
+    }  
+    
+    const projects = [];
+    
+    let storage: any = {},
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while ( i-- ) {
+        storage[keys[i]] =  localStorage.getItem(keys[i]);
+    }
+    for (const [key, value] of Object.entries(storage)) {
+      if(key.startsWith('p')){
+        projects.push(value);
+      }
+      
+    }
+    return projects;
+  }
+
+  
+
+}
