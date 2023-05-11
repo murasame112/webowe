f3bd321f3bf1cd73dfa332a27771a650dbe7ef0b
murasame112 <tomaszwiesek00@gmail.com>
2023-05-11 14:40:37 +0200

    project list page

--- ManageMe/src/app/new-project/new-project.component.ts
+++ ManageMe/src/app/new-project/new-project.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { ProjectService } from 'src/app/project.service';
+import {Project} from 'src/models/project.model';
 
 @Component({
   selector: 'app-new-project',
   templateUrl: './new-project.component.html',
   styleUrls: ['./new-project.component.scss']
 })
-export class NewProjectComponent {
+export class NewProjectComponent implements OnInit {
+  constructor(private projectService: ProjectService) {}
+  //@Input() project:Project|undefined;
+  ngOnInit(): void {    
+    // let project = this.ProjectService.createProject('newNewNewNewNew project', 'awangardowy opis czwartego projektu', false);
+    
+    //this.ProjectService.saveProject(project);
+  }
 
 }
--- ManageMe/src/app/project-list/project-list.component.ts
+++ ManageMe/src/app/project-list/project-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { ProjectService } from 'src/app/project.service';
+import {Project} from 'src/models/project.model';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss']
 })
-export class ProjectListComponent {
+export class ProjectListComponent implements OnInit {
+  constructor(private projectService: ProjectService) {}
+  @Input() projects:Array<Project> = [];
+  @Input() projectsUndef:Array<Project> | undefined;
+  ngOnInit(): void {    
 
+    this.projectsUndef = this.projectService.getProjects(); 
+    if (this.projectsUndef != undefined || this.projectsUndef === null) {
+      this.projects = this.projectsUndef;
+    }
+  }
 }
