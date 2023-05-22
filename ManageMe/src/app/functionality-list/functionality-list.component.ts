e3150aa9b42a37da6809172f7960e1d385d9d4fe
murasame112 <tomaszwiesek00@gmail.com>
2023-05-22 13:07:05 +0200

    mvp i func list (bez project name)

--- ManageMe/src/app/functionality-list/functionality-list.component.ts
+++ ManageMe/src/app/functionality-list/functionality-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { ProjectService } from 'src/app/project.service';
+import { Functionality} from 'src/models/functionality.model';
+import { FunctionalityService } from '../functionality.service';
 
 @Component({
   selector: 'app-functionality-list',
   templateUrl: './functionality-list.component.html',
   styleUrls: ['./functionality-list.component.scss']
 })
-export class FunctionalityListComponent {
+export class FunctionalityListComponent implements OnInit{
+  constructor(private functionalityService: FunctionalityService) {}
+  @Input() functionalities:Array<Functionality> = [];
+  @Input() functionalitiesUndef:Array<Functionality> | undefined;
+  ngOnInit(): void {    
 
+    this.functionalitiesUndef = this.functionalityService.getFunctionalities(); 
+    if (this.functionalitiesUndef != undefined || this.functionalitiesUndef === null) {
+      this.functionalities = this.functionalitiesUndef;
+    }
+  }
 }
