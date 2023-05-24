1fa7b0523292249ccbda9f5cd4fe08be71eb2c49
murasame112 <tomaszwiesek00@gmail.com>
2023-05-24 12:57:38 +0200

    project details

--- ManageMe/src/app/project-details/project-details.component.ts
+++ ManageMe/src/app/project-details/project-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input} from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
-import { Subscription } from 'rxjs'
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ProjectService } from 'src/app/project.service';
+import { Project } from 'src/models/project.model';
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
@@ -10,10 +12,12 @@ import { Subscription } from 'rxjs'
 export class ProjectDetailsComponent implements OnInit {
   protected projectKey!: string
   private routeSub!: Subscription
-  constructor(private readonly activatedRoute: ActivatedRoute ) {}
+  public prj!: Project;
+  constructor(private readonly activatedRoute: ActivatedRoute, private projectService: ProjectService ) {}
   ngOnInit(): void {    
     this.projectKey = this.activatedRoute.snapshot.params['key'];
     console.log(this.projectKey);
+    this.prj = this.projectService.getProjectByKey(this.projectKey);
 
   }
 }
