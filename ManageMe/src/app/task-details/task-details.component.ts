29305f573750354f7c781ab42713e9747f50969a
murasame112 <tomaszwiesek00@gmail.com>
2023-05-25 11:14:13 +0200

    task details

--- ManageMe/src/app/task-details/task-details.component.ts
+++ ManageMe/src/app/task-details/task-details.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TaskService } from 'src/app/task.service';
 import { Task } from 'src/models/task.model';
+import { FunctionalityService } from '../functionality.service';
+import { UserService } from '../user.service';
+import { Functionality } from 'src/models/functionality.model';
+import { User } from 'src/models/user.model';
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
@@ -10,12 +14,17 @@ import { Task } from 'src/models/task.model';
 export class TaskDetailsComponent implements OnInit {
   protected taskKey!: string
   public tsk!: Task;
+  public fun!: Functionality;
+  public own!: User;
   
-  constructor(private readonly activatedRoute: ActivatedRoute, private taskService: TaskService ) {}
+  constructor(private readonly activatedRoute: ActivatedRoute, private taskService: TaskService, private functionalityService: FunctionalityService, private userService: UserService) {}
 
   ngOnInit(): void {    
     this.taskKey = this.activatedRoute.snapshot.params['key'];
     this.tsk = this.taskService.getTaskByKey(this.taskKey);
+
+    this.fun = this.functionalityService.getFunctionalityByKey(this.tsk.functionalityKey as string);
+    this.own = this.userService.getUserByKey(this.tsk.ownerKey as string);
 
   }
 }
